cc85eb6a4baafaa0a11c067ea3b07577a72bb0a6
emilieanderton <emiliev99@hotmail.com>
2024-07-29 14:49:50 -0600

    Added a chunk of questions for array

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 // TODO: Include packages needed for this application
-
+import fs from "fs";
+import inquirer from "inquirer";
+import { type } from "os";
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation'
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
+    },
+    
+];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
